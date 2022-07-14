b9b966d6acdd1b4d5c3f4f40bc3e8418e9366c93
Алексей Макаренко <alexchb.makarenko@yandex.ru>
2022-07-14 20:49:27 +0300

    mille

---
+++ src/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+
+        int c = 3500;
+        int m = 20;
+        int bonus = c/m;
+        System.out.println("Количество баллов:" + bonus);
+    }
+}
